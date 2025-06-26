1ad8e9940d685f003fb9c30f258c59eda437f784
Ivan Klassen <ivanklassen1@gmail.com>
2025-06-27 01:54:06 +0700

    first commit

---
+++ frontend/js/main.js
@@ -0,0 +1,6 @@
+let createPost = document.querySelector('.create-post-btn')
+
+createPost.addEventListener('click', function (){
+    window.location.href = '../CreatePost.html'
+    console.log(1)
+})
