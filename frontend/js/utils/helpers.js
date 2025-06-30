9ed4f4d852c152d5974dc5a57e3c0412e272d09c
Semyon Berezovskiy <ber2semen@gmail.com>
2025-06-30 08:54:47 +0700

    убрал случайный фронт
    использовал для тестирования
    убрал - чтобы проще слить

--- frontend/js/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-/**
- * Перенаправляет на указанную страницу
- * @param {string} path - Путь для перенаправления (например, '/dashboard.html')
- */
-export function redirectTo(path) {
-    window.location.href = window.location.origin + path;
-}
